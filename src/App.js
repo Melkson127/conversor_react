33c89188bc317d287811db3ccca37b723036826b
Melkson Albuquerque <melkson150@gmail.com>
2021-04-28 15:20:07 -0300

    Correçao simbolos

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import Conversor from './components/Conversor'
 function App() {
   const linkLista = `https://free.currconv.com/api/v7/currencies?apiKey=6db3b3dc6b62d3927205`
   function currencySymbol(moeda){
-    const simbolo = fetch(linkLista).then(res=>res.json()).then(json=>json.results[moeda].currencySymbol)
-    return simbolo
+    return fetch(linkLista).then(res=>res.json()).then(json=>json.results[moeda].currencySymbol)
+  
   }
-    const lista = (idelement, idelement2)=>{
-      fetch(linkLista).then(res=>res.json())
-      .then(json=>{
-        Object.keys(json.results).forEach(ob=>{
-          const {currencyName, id} = json.results[ob]
-          document.getElementById(idelement).innerHTML += `<option value='${ob}'>${currencyName} - ${id}</option>`
-          document.getElementById(idelement2).innerHTML += `<option value='${ob}'>${currencyName} - ${id}</option>`
-        })
-      }).catch(err=>console.log(err))
-    }
+  const lista = (idelement, idelement2)=>{
+    fetch(linkLista).then(res=>res.json())
+    .then(json=>{
+      Object.keys(json.results).forEach(ob=>{
+        const {currencyName, id} = json.results[ob]
+        document.getElementById(idelement).innerHTML += `<option value='${ob}'>${currencyName} - ${id}</option>`
+        document.getElementById(idelement2).innerHTML += `<option value='${ob}'>${currencyName} - ${id}</option>`
+      })
+    }).catch(err=>console.log(err))
+  }
   return (
     <div className="App" onLoad={lista('moedaA', 'moedaB')} >
       <h1>Conversor de Moedas</h1>
